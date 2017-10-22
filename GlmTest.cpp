a54c43e74d6599edfb1c771e3bf0d6e9532312b5
Anton Firszov <anton.firszov@sapagroup.com>
2017-10-23 01:28:55 +0200

    can CAPTURE all vectors with Catch

--- GlmTest.cpp
+++ GlmTest.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define GLM_FORCE_MESSAGES
 
 #include <glm/vec2.hpp>
 #include <glm/vec4.hpp>
@@ -22,16 +23,73 @@ TEST_CASE("hello GLM")
     FAIL();
 }
 
+TEST_CASE("print and fail")
+{
+    using namespace glm;
+    
+    SECTION("vec4")
+    {
+        vec4 v;
+        CAPTURE(v);
+        FAIL();
+    }
+
+    SECTION("dvec4")
+    {
+        dvec4 v;
+        CAPTURE(v);
+        FAIL();
+    }
+
+    SECTION("aligned_vec4")
+    {
+        aligned_vec4 v;
+        CAPTURE(v);
+        FAIL();
+    }
+
+    SECTION("aligned_dvec4")
+    {
+        aligned_dvec4 v;
+        CAPTURE(v);
+        FAIL();
+    }
+}
+
 TEST_CASE("hello aligned")
 {
-    glm::aligned_vec4 a(1, 2, 3, 4);
-    glm::aligned_vec4 b(5, 6, 7, 8);
+    SECTION("4 x float")
+    {
+        glm::aligned_vec4 a(1.f, 2.f, 3.f, 4.f);
+        glm::aligned_vec4 b(5.f, 6.f, 7.f, 8.f);
 
-    glm::aligned_vec4 c = a + b;
+        CAPTURE(a);
+        CAPTURE(b);
+
+        glm::aligned_vec4 c = a + b;
+
+        REQUIRE(c == glm::aligned_vec4(7.0, 8, 10, 12));
+
+        glm::dvec4 foo = c;
+
+        REQUIRE(foo == glm::dvec4(6, 8, 10, 12));
+    }
     
-    REQUIRE(c == glm::aligned_vec4(7, 8, 10, 12));
+    SECTION("2 x double")
+    {
+        glm::aligned_dvec2 a(1.0, 2);
+        glm::aligned_dvec2 b(5.0, 6);
 
-    glm::dvec4 foo = c;
+        glm::aligned_dvec2 c = a + b;
+        REQUIRE(c == glm::aligned_dvec2(6.0, 8));
+    }
 
-    REQUIRE(foo == glm::dvec4(6, 8, 10, 12));
+    SECTION("4 x double")
+    {
+        glm::aligned_dvec4 a(1.0, 2, 3, 4);
+        glm::aligned_dvec4 b(5.0, 6, 7, 8);
+
+        glm::aligned_dvec4 c = a + b;
+        REQUIRE(c == glm::aligned_dvec4(6.0, 8, 10, 12));
+    }
 }
--- TestUtils.hpp
+++ TestUtils.hpp
@@ -3,6 +3,7 @@
 #include <glm/vec2.hpp>
 #include <glm/vec4.hpp>
 #include "catch.hpp"
+#include "glm/gtc/type_aligned.hpp"
 
 namespace Catch {
     template<> 
@@ -15,13 +16,25 @@ namespace Catch {
         }
     };
 
-    template<>
-    struct StringMaker<glm::dvec4>
+    namespace detail
     {
-        static std::string convert(glm::dvec4 const& v) {
-            std::ostringstream s;
-            s << '(' << v.x << ',' << v.y << ',' << v.z << ',' << v.w << ')';
-            return s.str();
-        }
-    };
+        template<class T>
+        struct Vector4StringMakerBase
+        {
+            static std::string convert(T const& v) {
+                std::ostringstream s;
+                s << '(' << v.x << ',' << v.y << ',' << v.z << ',' << v.w << ')';
+                return s.str();
+            }
+        };
+    }
+    
+#define MAKE_VECTOR4_STRING_MAKER(T) template<> struct StringMaker<T> : detail::Vector4StringMakerBase<T> {};
+
+    MAKE_VECTOR4_STRING_MAKER(glm::vec4)
+    MAKE_VECTOR4_STRING_MAKER(glm::dvec4)
+    MAKE_VECTOR4_STRING_MAKER(glm::aligned_vec4)
+    MAKE_VECTOR4_STRING_MAKER(glm::aligned_dvec4)
+
+#undef MAKE_VECTOR4_STRING_MAKER
 }
